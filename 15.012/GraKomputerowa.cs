8a84264110cd71f792d156312bead4c555621604
programista <programista@DESKTOP-B66160U>
2025-01-15 13:16:26 +0100

    Dodaj pliki projektów.

---
+++ 15.012/GraKomputerowa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._012
+{
+
+    public class GraKomputerowa
+    {
+       public static int wszystkieGry { get; set; }
+        private string _nazwa;
+        public string Nazwa
+        {
+            get { return _nazwa; }
+            set { _nazwa = value; }
+        }
+
+
+        //public string Nazwa { get; set; }
+        public string Gatunek { get; set; }
+        public int Wiek { get; set; }
+
+        public bool Multiplayer { get; set; }
+        public int id { get; set; }
+        public GraKomputerowa(string nazwa, string gatunek, int wiek, bool multiplayer)
+        {
+            Nazwa = nazwa;
+            Gatunek = gatunek;
+            Wiek = wiek;
+            Multiplayer = multiplayer;
+            id = wszystkieGry;
+            wszystkieGry++;
+
+            
+           
+        }
+    }
+}
---
+++ 15.012/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _15._012
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public ObservableCollection<GraKomputerowa> GryKomputerowe { get; set; }
+        public ObservableCollection<GraKomputerowa> WybraneGryKomputerowe { get; set; } = new ObservableCollection<GraKomputerowa>();
+        public List<string> gatunki { get; set; } = new List<string>() { "rpg", "fantasy", "zręcznościowa", "fps" };
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KolumnaGatunek.ItemsSource = gatunki;
+
+            wypelnijDane();
+
+            DataContext = this;
+        }
+
+        private void wypelnijDane()
+        {
+            GryKomputerowe = new ObservableCollection<GraKomputerowa>();
+            GryKomputerowe.Add(new GraKomputerowa("CS2", "fps", 18, true));
+            GryKomputerowe.Add(new GraKomputerowa("Tetris", "zręcznościowa", 6, false));
+            GryKomputerowe.Add(new GraKomputerowa("LoL", "fantasy", 13, true));
+            GryKomputerowe.Add(new GraKomputerowa("Fortnite", "fps", 12, true));
+            GryKomputerowe.Add(new GraKomputerowa("Cyberpunk", "rpg", 18, false));
+
+        }
+
+        private void Button_Click_DodaJgRE(object sender, RoutedEventArgs e)
+        {
+            string nazwa = nazwa_gry_textbox.Text;
+            string kategoria = gatunki_combo_box.Text;
+            int wiek;
+            if (int.TryParse(wiek_textbox.Text, out wiek))
+            {
+                if (multi_checkbox.IsChecked == true)
+                {
+                    GryKomputerowe.Add(new GraKomputerowa(nazwa, kategoria, wiek, true));
+                }
+                else
+                {
+                    GryKomputerowe.Add(new GraKomputerowa(nazwa, kategoria, wiek, false));
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wiek musi być liczbą");
+            }
+        }
+
+        private void Button_Click_Wyswietl_Kategorie(object sender, RoutedEventArgs e)
+        {
+            string gatunek = kategoria_comboBox.Text;
+            WybraneGryKomputerowe.Clear();
+            for (int i = 0; i < GryKomputerowe.Count; i++)
+            {
+                if (GryKomputerowe[i].Gatunek == gatunek)
+                {
+                    WybraneGryKomputerowe.Add(GryKomputerowe[i]);
+                }
+            }
+        }
+
+        private void Button_Click_Usuwanie(object sender, RoutedEventArgs e)
+        {
+            int numer = 0;
+            if (int.TryParse(numerGry.Text, out numer))
+                for (int i = 0; i < GryKomputerowe.Count; i++)
+            {
+                    if (GryKomputerowe[i].id == numer)
+                    {
+                        GryKomputerowe.Remove(GryKomputerowe[i]);
+                    }
+
+
+
+            }
+        }
+    }
+}
